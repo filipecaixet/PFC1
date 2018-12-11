7e33d09c74f63bf52b42ec4483557c3a3b169810
filipecaixet <37559453+filipecaixet@users.noreply.github.com>
2018-12-11 10:30:25 -0200

    código sensor de distancia atualizado

--- sensordedistancia/sensordedistancia.h
+++ sensordedistancia/sensordedistancia.h
@@ -1,10 +1,31 @@
 #ifndef sensordedistancia_h
 #define sensordedistancia_h
+#define sensor1 27
+#define sensor2 25
+#define sensor3 23
+#define sensor4 31
+#define sensor5 29
+#define sensor6 34
+#define sensor7 44
+#define sensor8 33
+#define sensor9 35
+
+
+//#include <map>
+//#include <string>
+
+// using namespace std;
+
+//std::map<int, std::string> sensores;
+//sensores[23] = "sensor 1";
+
 
 #include "Arduino.h"
 
-void inicializaInfra();
+void inicializaInfra(int);
 
-int imprimeEstado();
+int estado(int);
+
+void imprimeEstado(int);
 
 #endif
